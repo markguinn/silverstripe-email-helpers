9a4be85d1b16cf187c0758eee019440b75e1116e
helpfulrobot <chris@silverstripe.com>
2015-12-18 08:03:36 +1300

    Converted to PSR-2

--- code/StyledHtmlEmail.php
+++ code/StyledHtmlEmail.php
@@ -8,38 +8,39 @@ use Pelago\Emogrifier;
  */
 class StyledHtmlEmail extends Email
 {
-	/**
-	 * Replaces the default mail handling with a check for inline styles. If found
-	 * we run the email through emogrifier to inline the styles.
-	 * @param bool $isPlain
-	 * @return $this
-	 */
-	protected function parseVariables($isPlain = false) {
-		parent::parseVariables($isPlain);
+    /**
+     * Replaces the default mail handling with a check for inline styles. If found
+     * we run the email through emogrifier to inline the styles.
+     * @param bool $isPlain
+     * @return $this
+     */
+    protected function parseVariables($isPlain = false)
+    {
+        parent::parseVariables($isPlain);
 
-		// if it's an html email, filter it through emogrifier
-		if (!$isPlain && preg_match('/<style[^>]*>(?:<\!--)?(.*)(?:-->)?<\/style>/ims', $this->body, $match)){
-			$css = $match[1];
-			$html = str_replace(
-				array(
-					"<p>\n<table>",
-					"</table>\n</p>",
-					'&copy ',
-					$match[0],
-				),
-				array(
-					"<table>",
-					"</table>",
-					'',
-					'',
-				), 
-				$this->body
-			);
+        // if it's an html email, filter it through emogrifier
+        if (!$isPlain && preg_match('/<style[^>]*>(?:<\!--)?(.*)(?:-->)?<\/style>/ims', $this->body, $match)) {
+            $css = $match[1];
+            $html = str_replace(
+                array(
+                    "<p>\n<table>",
+                    "</table>\n</p>",
+                    '&copy ',
+                    $match[0],
+                ),
+                array(
+                    "<table>",
+                    "</table>",
+                    '',
+                    '',
+                ),
+                $this->body
+            );
 
-			$emog = new Emogrifier($html, $css);
-			$this->body = $emog->emogrify();
-		}
+            $emog = new Emogrifier($html, $css);
+            $this->body = $emog->emogrify();
+        }
 
-		return $this;
-	}
+        return $this;
+    }
 }
